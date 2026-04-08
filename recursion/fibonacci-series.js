edd7f40d98ff88381d85c2653a61b2024acad254
Pankaj Ghosh <dev.pankajghosh@gmail.com>
2026-04-08 15:39:52 +0530

    Update Fibonacci series script to read input correctly and output the series; modify input/output files

--- recursion/fibonacci-series.js
+++ recursion/fibonacci-series.js
@@ -1,21 +1,23 @@
 /**
- * Print Fibonacci series up to nth term
- *
- * Input: N = 5 (upto 5th term)
+ * Print Fibonacci series up to nth term using recursion
+ * Input: N = 5
  * Output: 0 1 1 2 3 5
  */
 
 import fs from "fs";
 
 (() => {
-  let str = String(fs.readFileSync("/dev/stdin", "utf8"));
+  let n = Number(fs.readFileSync("/dev/stdin", "utf8"));
 
-  function isPalindrome(i) {
-    if (i >= str.length / 2) return "Palindrome";
-    if (str[i] != str[str.length - i - 1]) return "Not Palindrome";
-
-    return isPalindrome(i + 1);
+  function fib(n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
   }
 
-  console.log(isPalindrome(0));
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(fib(i));
+  }
+
+  console.log(result.join(" "));
 })();
